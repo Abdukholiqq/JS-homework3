747536a2a99578d670c4673a3a15f9320688bdea
Abdukholiqq <sumusddollar@gmail.com>
2022-11-19 19:02:50 +0500

    second

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@
 var name = prompt('Ismingizni kiriting');
 let ball = +prompt (" To'plagan balingizni kiriting");
 
-if (0 < ball && ball < 80) {
+if (0 <= ball && ball < 80) {
   console.log(`${name} Afsuski siz o'qishga qabul qilinmadingiz`);
 } else if (80 <= ball && ball < 100) {
   alert("siz super-kontrakt asosida o'qishga tavsiya qilindingiz");
   var money = +prompt(
     " Super-kontrakt miqdori 3000$ iltimos pulingizni kiriting"
   );
-  if (money > 3000) {
+  if (money => 3000) {
     console.log(
       " tabriklaymiz siz super-kontrakat asosoada talabalikka qabul qilindingiz"
     );
@@ -20,7 +20,7 @@ if (0 < ball && ball < 80) {
 } else if (100 <= ball && ball < 150) {
   alert("siz kontrakt asosida o'qishga tavsiya qilindingiz");
   var money = +prompt(" kontrakt miqdori 2000$ iltimos pulingizni  kiriting");
-  if (money > 2000) {
+  if (money => 2000) {
     console.log(
       " tabriklaymiz siz kontrakat asosoada talabalikka qabul qilindingiz"
     );
@@ -31,11 +31,12 @@ if (0 < ball && ball < 80) {
   console.log(
     `Tabriklaymiz  ${name} siz grand asosida talabalikka qabul qilindingiz `
   );
-} else if (ball >= 180) {
-  console.log(
-    `Noto'g'ri raqam! iltimos  ballingizni tekshirib qaytadan kiriting `
-  );
 }
+//  else if (ball >= 180 && ball< 0) {
+//   console.log(
+//     `Noto'g'ri raqam! iltimos  ballingizni tekshirib qaytadan kiriting `
+//   );
+// }
 
 // Homework2
 
